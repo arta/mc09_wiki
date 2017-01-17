1bfdd4d7da651e66dfec190058726551d72768e3
Martin Fencl <martin.fencl@gmail.com>
2017-01-17 09:59:19 -0600

    Authenticate articles controller and views.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,6 @@
 class ArticlesController < ApplicationController
   before_action :set_article, except: [:index, :new, :create]
+  before_action :authenticate_user!, except: [:index, :show]
 
   # GET /articles
   #   from: <a href='/articles'> .. </a>

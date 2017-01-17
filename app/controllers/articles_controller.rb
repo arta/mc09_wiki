fded5523d73a4fb2ca948d659ec1c84f9d54fd15
Martin Fencl <martin.fencl@gmail.com>
2017-01-16 20:18:26 -0600

    Add article create and update actions and views.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,43 +1,78 @@
 class ArticlesController < ApplicationController
 
   # GET /articles
-  #   =link_to .. articles_path
+  #   from: <a href='/articles'> .. </a>
+  #   by:   =link_to .. articles_path
   def index
     @articles = Article.order created_at: :desc
   end
-  # Automatically renders /articles/index (with @articles available)
+  # Automatically renders /articles/index haml view (with @articles available)
+  #   that renders /articles html index page
 
   # GET /articles/new
-  #   =link_to .. new_article_path
+  #   from: <a href='/articles/new'> .. </a>
+  #   by:   =link_to .. new_article_path
   def new
     @article = Article.new
   end
-  # Automatically renders /articles/new haml which renders /articles/_form haml
-  #   with contains =form_for @article .. with @article.new_record?
-  #   which renders /articles/new html which contains
+  # Automatically renders /articles/new haml view
+  #   that renders /articles/_form haml partial
+  #   that contains =form_for @article .. with @article.new_record?
+  #   all of which renders /articles/new html page that contains
   #   <form action='/articles', method='post'..>
   #     <input name='article[:title]', ..>
 
   # POST /articles
-  #   <form action='/articles' method='post'..>
-  #   =form_for @article .. if @article.new_record?
+  #   from: <form action='/articles' method='post'..>
+  #   by:   =form_for @article .. if @article.new_record?
   def create
     @article = Article.new article_params
     if @article.save
       redirect_to @article, notice:'Article created.'
     else
-      render :new
+      render 'new'
     end
   end
   # No view. Explicitly redirect_to|render action|view
 
   # GET /articles/:id
-  #   =link_to .. @article | article_path( @article )
+  #   from: <a href='/articles/1'> .. </a>
+  #   by:   =link_to .. @article | article_path( @article )
   #   the value of :id is extracted by router and assigned to params[:id]
   def show
     @article = Article.find params[:id]
   end
-  # Automatically renders /articles/show (with @article available)
+  # Automatically renders /articles/show haml view (with @article :id)
+  #   that renders /articles/1 html show page
+
+  # GET /articles/:id/edit
+  #   from: <a href='/articles/1/edit'> .. </a>
+  #   by:   =link_to .. [:edit, @article] | edit_article_path( @article )
+  #   router extracts the value of :id and assigns it to params[:id]
+  def edit
+    @article = Article.find params[:id]
+  end
+  # Automatically renders /articles/:id/edit haml view (with @article :id)
+  #   that renders /articles/_form haml partial
+  #   that contains =form_for @article .. with @article.persisted?
+  #   all of which renders /articles/1/edit html page that contains
+  #   <form action='/articles/1', method='patch' ..>
+  #     <input name='article[title]', ..>
+
+  # PATCH|PUT /articles/:id
+  #   from: <form action='/articles/1', method='patch' ..>
+  #   by:   =form_for @article .. if @article.persisted?
+  #   router extracts :id value from the request and assigns it to params[:id]
+  def update
+    @article = Article.find params[:id]
+
+    if @article.update article_params
+      redirect_to @article, notice:'Article updated.'
+    else
+      render 'edit'
+    end
+  end
+  # No view. Explicitly redirect_to|render action|view
 
   private
     def article_params
